a154100fd3d7afd7c18bbcd653316dc19e425b50
aerits <stev_nm@protonmail.com>
2023-05-09 21:57:38 -0600

    djsafkl

--- emacs.el
+++ emacs.el
@@ -215,6 +215,15 @@
   :ensure t)
 (ac-config-default)
 
+;; icons ;;
+(use-package nerd-icons
+  :ensure t
+  ;; :custom
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  ;; (nerd-icons-font-family "Symbols Nerd Font Mono")
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; extra config ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
