858dae63b2121856955d82152447a7bf5dae9ea1
aerits <stev_nm@protonmail.com>
2025-07-20 13:04:38 -0600

    gaming

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   outputs = { self, nixpkgs }: {
     nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [ ./configuration.nix ./hardware-configuration.nix ];
     };
   };
 }
