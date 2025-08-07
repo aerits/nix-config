d00103dfecc7069d3e063750506b5c9dba0f7eae
aerits <stev_nm@protonmail.com>
2025-08-07 08:22:41 -0600

    added lix and updated flake lock

--- flake.nix
+++ flake.nix
@@ -2,11 +2,23 @@
   description = "dicedmangoes's nixos config";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
+  inputs.lix = {
+    url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+    flake = false;
+  };
+
+  inputs.lix-module = {
+    url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.lix.follows = "lix";
+  };
   outputs =
     {
       self,
       nixpkgs,
       nixpkgs-xr,
+      lix,
+      lix-module,
     }:
     {
       nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
@@ -15,6 +27,7 @@
           ./hardware-configuration.nix
           ./fix-flatpaks.nix
           nixpkgs-xr.nixosModules.nixpkgs-xr
+          lix-module.nixosModules.default
         ];
       };
     };
