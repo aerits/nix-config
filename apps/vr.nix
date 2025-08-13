0106843913f46c14e6548f7978b9139e1d3002a5
aerits <stev_nm@protonmail.com>
2025-08-13 11:59:25 -0600

    fbt xrizer fork added

--- apps/vr.nix
+++ apps/vr.nix
@@ -6,6 +6,32 @@
 }:
 
 {
+  # xrizer with fbt
+  nixpkgs.overlays = [
+    (final: prev: {
+      xrizer = prev.xrizer.overrideAttrs {
+        src = final.fetchFromGitHub {
+          owner = "RinLovesYou";
+          repo = "xrizer";
+          # IMPORTANT: Fill the below field with the latest commit hash from https://github.com/RinLovesYou/xrizer/commits/experimental2 (click the Copy full SHA button on the right side)
+          rev = "f491eddd0d9839d85dbb773f61bd1096d5b004ef";
+          # IMPORTANT: Replace the below field with the correct hash, the error when building with this empty will give you the expected hash.
+          hash = "sha256-12M7rkTMbIwNY56Jc36nC08owVSPOr1eBu0xpJxikdw=";
+        };
+        cargoDeps = final.rustPlatform.fetchCargoVendor {
+          src = final.fetchFromGitHub {
+            # quick and dirty fix to have the right cargo hash
+            owner = "RinLovesYou";
+            repo = "xrizer";
+            rev = "f491eddd0d9839d85dbb773f61bd1096d5b004ef";
+            hash = "sha256-12M7rkTMbIwNY56Jc36nC08owVSPOr1eBu0xpJxikdw=";
+          };
+          hash = "sha256-87JcULH1tAA487VwKVBmXhYTXCdMoYM3gOQTkM53ehE=";
+        };
+
+      };
+    })
+  ];
 
   services.wivrn = {
     enable = true;
